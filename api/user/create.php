f205f30f799faec736ae268201e7cc9f11a7c273
larexx40 <larexx40@gmail.com>
2022-07-17 13:18:41 +0100

    Create User

---
+++ api/user/create.php
@@ -0,0 +1,34 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    include_once '../../models/users.php';
+
+    //instantiate user object
+    $users = new Users();
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    if(!empty($data->username)&& !empty($data->email) && !empty($data->age) && !empty($data->phoneno) && !empty($data->password) ){
+        $users->username = $data->username;
+        $users->email = $data->email;
+        $users->age = $data->age;
+        $users->phoneno = $data->phoneno;
+        $users->password = $data->password;
+
+        if($users->insertUser()){
+            http_response_code(200);
+            echo json_encode(array("message"=>"User created"));
+        }else{
+            http_response_code(503);
+            echo json_encode(array("message"=>"Iss with DB, Unable to create user"));
+        }
+
+    }else{
+        http_response_code(400);
+        echo json_encode(array("message"=> "Invalid input, Unable to Create User"));
+    }
+?>
